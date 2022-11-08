89b3052a2ef22e5e059755c2d2715f3c98959159
Scott Klein <sklein@thecity.nyc>
2022-11-08 16:39:06 -0500

    fixes newsletter signup link

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -7,7 +7,7 @@ export const Newsletter = () => (
         <p class="newsletter-intro">Sign up for our newsletter, The Daily Scoop, and get the latest stories from THE CITY delivered to your inbox each morning.</p>
       </div>
       <div class="newsletter-link">
-        <a href="https://nyc.us20.list-manage.com/subscribe/post?u=73d98c6dfc90032198ec7bdee&id=aa6c8f62b7">Subscribe</a>
+        <a href="https://nyc.us20.list-manage.com/subscribe?u=73d98c6dfc90032198ec7bdee&id=aa6c8f62b7">Subscribe</a>
       </div>
     </div>
   </div>
